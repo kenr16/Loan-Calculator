4132aa28b77c45dea73b04e357dfd27b68993284
Ken Rutan <kenr16@gmail.com>
2017-07-24 14:09:15 -0700

    add very basic bar graph to the page plot

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   var sampleSVG = d3.select("#viz")
       .append("svg")
       .attr("width", 1000)
-      .attr("height", 1000);
+      .attr("height", 500);
 
   sampleSVG.append("circle")
       .style("stroke", "gray")
@@ -47,6 +47,57 @@ $(document).ready(function(){
           .style("fill", "white")
           .each("end", animateThirdStep);
   };
+
+
+
+  var tomatoes = [
+    {"width":25,"height":83,"color":"red"},
+    {"width":25,"height":97,"color":"red"},
+    {"width":25,"height":77,"color":"red"},
+    {"width":25,"height":73,"color":"red"},
+    {"width":25,"height":89,"color":"red"},
+    {"width":25,"height":94,"color":"red"},
+    {"width":25,"height":97,"color":"red"},
+    {"width":25,"height":74,"color":"red"},
+    {"width":25,"height":91,"color":"red"},
+    {"width":25,"height":80,"color":"red"},
+    {"width":25,"height":86,"color":"red"},
+    {"width":25,"height":77,"color":"red"},
+    {"width":25,"height":80,"color":"red"},
+    {"width":25,"height":67,"color":"red"},
+    {"width":25,"height":79,"color":"red"},
+    {"width":25,"height":73,"color":"red"},
+    {"width":25,"height":84,"color":"red"},
+    {"width":25,"height":97,"color":"red"},
+    {"width":25,"height":98,"color":"red"},
+    {"width":25,"height":94,"color":"red"},
+  ]
+
+  var w= 535;
+  var h= 250;
+
+  var svg = d3.select("#graphDiv")
+      .append("svg")
+      .attr("width", 1000)
+      .attr("height", 1000);
+
+  svg.selectAll('rect.colorBar')
+      .data(tomatoes)
+      .enter()
+      .append('rect')
+      .attr('width', function(d,i){
+          return d.width
+      })
+      .attr('height', function(d,i){
+          return d.height*2
+      })
+      .attr('x', function(d,i){
+          return i * (d.width+2)
+      })
+      .attr('y', function(d,i){
+          return h - d.height*2
+      })
+      .attr('fill', 'red');
 
 
 
